11759a4c111571b622e7b6ebf84ee29731235f8a
Bruno Lima <brlimadev89@gmail.com>
2024-07-09 16:32:16 -0300

    Implementando JWT e Swagger Authorization

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { Usuario } from './model/usuario';
+import { UsuarioService } from './services/usuario.service';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +15,16 @@ import {MatGridListModule} from '@angular/material/grid-list';
   styleUrl: './app.component.css'
 
 })
-export class AppComponent {
-  title = 'Infnet';
+export class AppComponent implements OnInit {
+  userName = '';
+
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    if (sessionStorage.getItem("user_session")) {
+      let token = JSON.parse(sessionStorage.getItem("user_session") as string);
+      this.userName = token.name;
+    }
+
+  }
 }
